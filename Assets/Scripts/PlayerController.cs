71170a3fe62e9c96f1e4aacd27e88c285fce9e8f
Breno Sarkis <sarkisbreno@gmail.com>
2018-11-27 18:30:04 +0100

    Adds animator variable to control animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D theRB;
     public float moveSpeed;
 
+    public Animator animator; 
+
     // Use this for initialization
     void Start()
     {
@@ -14,6 +16,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+        var x = Input.GetAxisRaw("Horizontal");
+        var y = Input.GetAxisRaw("Vertical");
+        theRB.velocity = new Vector2(x, y) * moveSpeed;
+        animator.SetFloat("moveX", x);
+        animator.SetFloat("moveY", y);
     }
 }
