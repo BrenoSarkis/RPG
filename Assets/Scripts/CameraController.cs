174602bd379df8842da5c80a3036d298fc8427f9
Breno Sarkis <sarkisbreno@gmail.com>
2018-11-28 19:11:18 +0100

    Creates camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    void Start()
+    {
+        target = PlayerController.instance.transform;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+}
