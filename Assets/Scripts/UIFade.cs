4a058861ca74b846869950a03a5e6dccd1a76927
Breno Sarkis <sarkisbreno@gmail.com>
2018-11-29 20:39:01 +0100

    Adds proper methods to do fading

--- Assets/Scripts/UIFade.cs
+++ Assets/Scripts/UIFade.cs
@@ -1,16 +1,62 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
-public class UIFade : MonoBehaviour {
+public class UIFade : MonoBehaviour
+{
+    public static UIFade instance;
+    public Image fadeScreen;
+    public float fadeSpeed;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public bool shouldFadeToBlack;
+    public bool shouldFadeFromBlack;
+
+    void Start()
+    {
+        instance = this;
+    }
+
+    void Update()
+    {
+        if (shouldFadeToBlack)
+        {
+            fadeScreen.color = new Color(fadeScreen.color.r, fadeScreen.color.g, fadeScreen.color.b, Mathf.MoveTowards(fadeScreen.color.a, 1f, fadeSpeed * Time.deltaTime));
+
+            StopsFadingToBlack();
+        }
+
+        if (shouldFadeFromBlack)
+        {
+            fadeScreen.color = new Color(fadeScreen.color.r, fadeScreen.color.g, fadeScreen.color.b, Mathf.MoveTowards(fadeScreen.color.a, 0f, fadeSpeed * Time.deltaTime));
+
+            StopsFadingFromBlack();
+        }
+    }
+
+    private void StopsFadingFromBlack()
+    {
+        if (fadeScreen.color.a == 0f)
+        {
+            shouldFadeFromBlack = false;
+        }
+    }
+
+    private void StopsFadingToBlack()
+    {
+        if (fadeScreen.color.a == 1f)
+        {
+            shouldFadeToBlack = false;
+        }
+    }
+
+    public void FadeToBlack()
+    {
+        shouldFadeToBlack = true;
+        shouldFadeFromBlack = false;
+    }
+
+    public void FadeFromBlack()
+    {
+        shouldFadeFromBlack = true;
+        shouldFadeToBlack = false;
+    }
 }
