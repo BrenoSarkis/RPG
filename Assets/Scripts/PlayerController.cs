cbef5eb236a61490b653fd7bd4d130a4a285ad1c
Breno Sarkis <sarkisbreno@gmail.com>
2018-11-29 20:10:02 +0100

    Camera control and boundaries

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,13 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D theRB;
     public float moveSpeed;
-
     public Animator animator;
     public static PlayerController instance;
     public string areaTransitionName;
 
-    // Use this for initialization
+    private Vector3 bottomLeftLimit;
+    private Vector3 topRightLimit;
+
     void Start()
     {
         if (instance == null)
@@ -45,5 +46,15 @@ public class PlayerController : MonoBehaviour
             animator.SetFloat("lastMoveX",  x);
             animator.SetFloat("lastMoveY",  y);
         }
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, bottomLeftLimit.x, topRightLimit.x),
+                                         Mathf.Clamp(transform.position.y, bottomLeftLimit.y, topRightLimit.y),
+                                         transform.position.z);
+    }
+
+    public void SetBounds(Vector3 bottomLeft, Vector3 topRight)
+    {
+        bottomLeftLimit = bottomLeft + new Vector3(0.5f, 0.8f, 0f);
+        topRightLimit = topRight + new Vector3(-0.5f, -0.8f, 0f);
     }
 }
