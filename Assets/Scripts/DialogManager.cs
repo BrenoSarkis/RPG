1848a5ef13716cdf7d5f45940c0a6f8c374c6e32
Breno Sarkis <sarkisbreno@gmail.com>
2018-11-30 23:00:54 +0100

    Dialog manager

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    public Text dialogText;
+    public Text nameText;
+
+    public GameObject dialogBox;
+    public GameObject nameBox;
+
+    public string[] dialogLines;
+
+    public int currentLine;
+
+    void Start()
+    {
+        dialogText.text = dialogLines[currentLine];
+    }
+
+    void Update()
+    {
+        if (dialogBox.activeInHierarchy)
+        {
+            if (Input.GetButtonUp("Fire1"))
+            {
+                
+            }
+        }
+    }
+}
