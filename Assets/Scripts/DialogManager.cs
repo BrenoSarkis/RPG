79a0a834234ae92060fbd501eafd8b9a0795a394
Breno Sarkis <sarkisbreno@gmail.com>
2018-12-01 14:13:11 +0100

    Switch lines of fire button up

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -15,17 +15,17 @@ public class DialogManager : MonoBehaviour
 
     void Start()
     {
-        dialogText.text = dialogLines[currentLine];
+        //dialogText.text = dialogLines[currentLine];
     }
 
     void Update()
     {
-        if (dialogBox.activeInHierarchy)
-        {
-            if (Input.GetButtonUp("Fire1"))
-            {
+        //if (dialogBox.activeInHierarchy)
+        //{
+        //    if (Input.GetButtonUp("Fire1"))
+        //    {
                 
-            }
-        }
+        //    }
+        //}
     }
 }
