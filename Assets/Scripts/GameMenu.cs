823d246fa3578f37318b29a088dbbe2559155960
Breno Sarkis <sarkisbreno@gmail.com>
2018-12-03 19:49:34 +0100

    Sets char info on game menu script

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -1,8 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class GameMenu : MonoBehaviour
 {
     public GameObject menu;
+    public Text[] namesText;
+    public Text[] hpsText;
+    public Text[] mpsText;
+    public Text[] lvlsText;
+    public Text[] expsText;
+    public Slider[] expsSliders;
+    public Image[] charactersImages;
+    public GameObject[] charStatHolder;
+
+    private CharStats[] playerStats;
 
     void Start()
     {
@@ -22,7 +33,13 @@ public class GameMenu : MonoBehaviour
             {
                 menu.SetActive(true);
                 GameManager.instance.gameMenuOpen = true;
+                UpdateMainStats();
             }
         }
+    }
+
+    public void UpdateMainStats()
+    {
+        playerStats = GameManager.instance.playerStats;
     }
 }
