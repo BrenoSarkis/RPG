572863667419b38f7cd3488336134f73b66b60e7
brenosarkis <sarkisbreno@gmail.com>
2018-12-11 22:50:27 +0100

    Add press button function to item button

--- Assets/Scripts/ItemButton.cs
+++ Assets/Scripts/ItemButton.cs
@@ -16,4 +16,12 @@ public class ItemButton : MonoBehaviour
     {
 
     }
+
+    public void Press()
+    {
+        if (GameManager.instance.itensHeld[buttonValue] != "")
+        {
+          
+        }
+    }
 }
